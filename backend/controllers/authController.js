99e6df67ca87089856fbf9eca090571fb14bdb31
Peter Dinh <peterdinh94@gmail.com>
2023-03-28 16:42:54 -0700

    update authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -81,7 +81,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1];
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
-  }
+  } else token = req.header('x-auth-token');
 
   if (!token) {
     return next(
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,9 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+router.get('/logout', authController.logout);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get('/logout', authController.logout);
 router.post('/verifyToken', authController.verifyToken);
 
 module.exports = router;
